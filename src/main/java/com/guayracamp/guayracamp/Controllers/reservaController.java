d58bc0b1c1d5e873c14f93d6315cd42501513c37
CrisViera <cristianvieracruz@gmail.com>
2024-12-22 17:44:49 +0000

    Crear la vista index y las controladoras.

--- src/main/java/com/guayracamp/guayracamp/Controllers/reservaController.java
+++ src/main/java/com/guayracamp/guayracamp/Controllers/reservaController.java
@@ -2,6 +2,7 @@ package com.guayracamp.guayracamp.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class reservaController {
@@ -9,5 +10,11 @@ public class reservaController {
     @GetMapping("reserva")
     String reserva(){
         return "/reserva";
+    }
+    
+    // Manejo del formulario de reserva
+    @PostMapping("formularioReserva")
+    public String formularioReserva() {
+    	return "/reservaConfirmada";
     }
 }
--- src/main/java/com/guayracamp/guayracamp/GuayracampApplication.java
+++ src/main/java/com/guayracamp/guayracamp/GuayracampApplication.java
@@ -2,8 +2,10 @@ package com.guayracamp.guayracamp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.guayracamp")
 public class GuayracampApplication {
 
 	public static void main(String[] args) {
